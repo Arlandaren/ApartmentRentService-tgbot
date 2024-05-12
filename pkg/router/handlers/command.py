ad27b0b875bc8eff7f8f4de13be50d705e56ad5e
Arlandaren <bad-07@list.ru>
2024-05-13 00:00:23 +0900

    build: updating gitignore

--- pkg/router/handlers/command.py
+++
@@ -1,9 +0,0 @@
-from aiogram.filters import CommandStart
-from aiogram.types import Message
-from aiogram import Router
-
-
-r = Router()
-@r.message(CommandStart())
-async def start(msg:Message):
-    await msg.answer("Привет")
